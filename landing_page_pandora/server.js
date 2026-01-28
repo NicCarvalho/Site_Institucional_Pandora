0856c42d367e2a4663ff07804640a238f5005abe
Shausinaguer <149312021+Shausinaguer@users.noreply.github.com>
2026-01-28 00:57:50 -0300

    [update] landing_page v2.0

--- landing_page_pandora/server.js
+++ landing_page_pandora/server.js
@@ -6,7 +6,7 @@ const dev = false
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 21040
 
 app.prepare().then(() => {
     createServer((req, res) => {
